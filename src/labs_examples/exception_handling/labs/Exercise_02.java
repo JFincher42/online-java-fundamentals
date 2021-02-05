b49090aff60818697c67af1a7c695fdd80c785fe
Jon Fincher <jfincher42@gmail.com>
2021-02-05 16:53:12 -0600

    Exceptional exceptions

--- src/labs_examples/exception_handling/labs/Exercise_02.java
+++ src/labs_examples/exception_handling/labs/Exercise_02.java
@@ -1,5 +1,8 @@
 package labs_examples.exception_handling.labs;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Exception Handling Exercise 2:
  *
@@ -8,3 +11,23 @@ package labs_examples.exception_handling.labs;
  */
 
 
+public class Exercise_02 {
+
+    public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);
+        int num, den, res;
+
+        try {
+            System.out.print("Enter a number: ");
+            num = kbd.nextInt();
+            System.out.print("Enter another number: ");
+            den = kbd.nextInt();
+            System.out.println(num / den);
+
+        } catch (InputMismatchException e){
+            System.out.println("You must enter a number.");
+        } catch (ArithmeticException e) {
+            System.out.println("The second number must not be zero.");
+        }
+    }
+}
