95d14e71aff40505236a021c07288c64b7b45897
Jon Fincher <jfincher42@gmail.com>
2021-07-05 10:42:44 -0500

    Scratch file for testing

---
+++ src/jonf/testString.java
@@ -0,0 +1,9 @@
+package jonf;
+
+public class testString {
+    public static void main(String[] args) {
+        String testWord = "abcdefghijkl";
+
+        System.out.println(testWord.substring(testWord.length()-3));
+    }
+}
